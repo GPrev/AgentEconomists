1a0de84d6ba647df998556a2fb350cdcceda7ab2
Romain Lagneau <ro.lagneau@laposte.net>
2021-06-20 13:45:15 +0200

    [CORPS] Créations de ActionDescription

---
+++ AgentEconomistsLib/ActionDescription.cpp
@@ -0,0 +1,5 @@
+#include "ActionDescription.h"
+
+agenteconomists::ActionDescription::ActionDescription()
+{
+}
---
+++ AgentEconomistsLib/ActionDescription.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "AgentEconomistsLib.h"
+
+// Ignore warning 4251 (use of STL in a DLL)
+#pragma warning( push )
+#pragma warning( disable : 4251 )
+
+namespace agenteconomists
+{
+	/// <summary>
+	/// Class that describes the actions that can be perform in the simulation.
+	/// </summary>
+	class AELIB_API ActionDescription
+	{
+	public:
+		ActionDescription();
+
+	private:
+
+	};
+}
+
+#pragma warning(pop)
+
